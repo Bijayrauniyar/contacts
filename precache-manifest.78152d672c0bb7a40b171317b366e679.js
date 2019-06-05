0a2d74903f7ac76eb0d62cdc60d828e548f101d9
Bijay Rauniyar <bijay@tekkon.io>
2019-06-05 13:19:47 +0545

    first-commit

---
+++ precache-manifest.78152d672c0bb7a40b171317b366e679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3558021d1e3a498a7a11de98e6fc5282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f951bc0ad49731f4bda4",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0d9a24ceb4fee2b469d0",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f951bc0ad49731f4bda4",
+    "url": "/static/js/2.fd5ab474.chunk.js"
+  },
+  {
+    "revision": "0d9a24ceb4fee2b469d0",
+    "url": "/static/js/main.89da8c39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
